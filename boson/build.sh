87c2fb77390565ff1e081c3010d00a3dc12031d2
Weston-Tyler <150041718+Weston-Tyler@users.noreply.github.com>
2024-08-06 16:50:39 +0000

    Initial commit

---
+++ boson/build.sh
@@ -0,0 +1,1 @@
+docker build -t us-central1-docker.pkg.dev/double-catfish-291717/seerai-docker/images/gfw:v0.0.${1} -f Dockerfile .
---
+++ boson/push.sh
@@ -0,0 +1,1 @@
+docker push us-central1-docker.pkg.dev/double-catfish-291717/seerai-docker/images/gfw:v0.0.${1}
